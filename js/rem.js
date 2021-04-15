07e733294ebf6b963a25c6e1089f351df7a42b8e
xiao-tiana <2421265015@qq.com>
2021-04-15 08:57:51 +0800

    养老信息-发布消息

---
+++ js/rem.js
@@ -0,0 +1,17 @@
+(function(doc,win){
+    var docEl = doc.documentElement,resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+    recalc = function(){
+        var clientWidth = docEl.clientWidth;
+        if(!clientWidth) return;
+        if(clientWidth>=640){
+            docEl.style.fontSize = '100px'; //1rem  = 100px
+            // document.documentElement.style.fontSize = "100px" 相当于根节点的字体大小
+        }
+        else{
+            docEl.style.fontSize = 100 * (clientWidth / 640) + 'px';
+        }
+    };
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document,window);
